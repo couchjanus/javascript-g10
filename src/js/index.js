b2711b182bf41dcea911043bc3ea3db171fb73c1
couch Janus <couchjanus@gmail.com>
2018-10-12 16:24:36 +0300

    added unit 2

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+    /*  
+    document.getElementById("cart-toggle").addEventListener('click', function () {
+          document.getElementById("cart-sidebar").classList.add('show-sidebar');
+          }
+      );
+    */
+    /* 
+    document.querySelector("#cart-sidebar .remove").addEventListener('click', function () {
+        document.getElementById("cart-sidebar").classList.remove('show-sidebar');
+        }
+    );
+    */
+    /* 
+      document.querySelector("#cart-sidebar .remove").addEventListener('click', function () {
+        if (document.getElementById("cart-sidebar").classList.contains("show-sidebar")) {
+
+          document.getElementById("cart-sidebar").classList.remove('show-sidebar');
+          }
+        }
+      );
+    */
+    // var hamburgerToggle = document.querySelector('.trigger');
+      
+    //     hamburgerToggle.addEventListener('click', function(e){
+    //         e.preventDefault();
+                
+    //         document.querySelector('.flexmenu').classList.toggle('active');
+    //     });
+    
+    // var megamenuToggle = document.querySelector('.submenu');
+      
+    // megamenuToggle.addEventListener('click', function(e){
+    //         e.preventDefault();
+    
+    //         document.querySelector('.megamenu').classList.toggle('active-mega');
+            
+    //     });
+
+    
+}
